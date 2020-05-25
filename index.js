8a4aa831fe78d2fbcd042ca57864af3087f8dca6
shubhamgoel7703 <shubhamgoel7703@gmail.com>
2020-05-25 20:36:33 +0530

    Update index.js
    fft added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// websocket port : 2222
+// mqtt port : 1234
+
 // MQTT broker
 var mosca = require('mosca')
 
@@ -16,14 +19,21 @@ broker.on('clientConnected', (client) => {
 
 
 broker.on('published', (packet) => {
-    message = packet.payload.toString()
-    console.log("published ", message)
-    // console.log("published packet", packet)
 
-    if (packet.topic == 'vibrationDataTopic') {
-        io.sockets.emit('sendData', message);
+    try {
+        message = packet.payload.toString()
+        //console.log("published message", message);
+
+        if (packet.topic == 'vibrationDataTopic') {
+            io.sockets.emit('sendData', message);
+            pushAndEmitFFTData(JSON.parse(message).d);
+        }
     }
-})
+    catch (ex) {
+        console.log("exception ", ex);
+    }
+});
+
 
 broker.on('subscribed', (subscribedTopic) => {
     // message = packet.payload.toString()
@@ -32,7 +42,76 @@ broker.on('subscribed', (subscribedTopic) => {
 
 
 
+function pushAndEmitFFTData(message) {
+    //console.log("fft message", message);
 
+    if (message.Acceleration_X_Node_0 != undefined) {
+        console.log("message.Acceleration_X_Node_0", message.Acceleration_X_Node_0);
+        fftXArray.push(message.Acceleration_X_Node_0)
+        arrayShiftingLogic(fftXArray);
+    }
+
+    if (message.Acceleration_Y_Node_0 != undefined) {
+        fftYArray.push(message.Acceleration_Y_Node_0)
+        arrayShiftingLogic(fftYArray);
+    }
+
+    if (message.Acceleration_Z_Node_0 != undefined) {
+        fftZArray.push(message.Acceleration_Z_Node_0)
+        arrayShiftingLogic(fftZArray);
+    }
+
+    console.log({ fftXArray: fftXArray, fftYArray: fftYArray, fftZArray: fftZArray });
+
+    let fft_X = [];
+    if (fftXArray.length >= 8) {
+        fft.forward(fftXArray);
+        fft_X = [...fft.spectrum];
+    }
+    else
+        console.log("fftXArray ", fftXArray);
+
+    let fft_Y = [];
+    if (fftYArray.length >= 8) {
+        fft.forward(fftYArray);
+        fft_Y = [...fft.spectrum]
+    }
+
+    let fft_Z = [];
+    if (fftZArray.length >= 8) {
+        fft.forward(fftZArray);
+        fft_Z = [...fft.spectrum]
+    }
+
+    if (fftZArray.length >= 8) {
+        console.log({ fft_X: fft_X, fft_Y: fft_Y, fft_Z: fft_Z });
+        io.sockets.emit('sendFFTData', { fft_X: fft_X, fft_Y: fft_Y, fft_Z: fft_Z });
+        fftXArray = [];
+        fftYArray = [];
+        fftZArray = [];
+    }
+}
+
+
+function arrayShiftingLogic(array) {
+    if (array.length > fftBufferSize) {
+        array.shift(); // removes the first element from an array 
+    }
+}
+
+
+var lib = require('dsp.js');
+var fft = new lib.FFT(8, 44100);
+fftBufferSize = 8;
+fftXArray = [];
+fftYArray = [];
+fftZArray = [];
+
+
+
+
+// {"d":{"myName":"Node6LoWPAN","MAX_Temperature_Node_0":29.30,"Button_Counter_Node_0":0,
+//   "Temperature_Node_0":29.40,"Humidity_Node_0":68.40,"Acceleration_Z_Node_0":0.98}}
 
 
 var express = require('express');
